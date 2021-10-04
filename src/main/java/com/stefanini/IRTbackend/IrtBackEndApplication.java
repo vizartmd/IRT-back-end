afcebf165784c2be9548ba619f52c40c2a99c465
stanos <1991.andrei@gmail.com>
2021-10-04 09:26:12 +0300

    Initial commit

---
+++ src/main/java/com/stefanini/IRTbackend/IrtBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.stefanini.IRTbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrtBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrtBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefanini/IRTbackend/IrtBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanini.IRTbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrtBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
