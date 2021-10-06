9f13e23578bcf987011606f38ac5dee7b27e03c6
SandraRusu17 <sandra.rusu17@gmail.com>
2021-10-06 09:42:01 +0300

    DataSource and EntityManager configuration for DataBase connection

---
+++ src/main/java/com/stefanini/IRTbackend/entity/User.java
@@ -0,0 +1,54 @@
+package com.stefanini.IRTbackend.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private long id;
+
+    @Column(name = "task_title")
+    private String taskTitle;
+
+    @Column(name = "task_description")
+    private String taskDescription;
+
+    public User(long id, String taskTitle, String taskDescription) {
+        this.id = id;
+        this.taskTitle = taskTitle;
+        this.taskDescription = taskDescription;
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+}
